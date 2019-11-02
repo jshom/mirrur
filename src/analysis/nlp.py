318a7be015f455138f8afed872cbb2e04c1afa2a
tupleu <57295844+tupleu@users.noreply.github.com>
2019-11-02 15:47:23 -0400

    added nlp.py

---
+++ src/analysis/nlp.py
@@ -0,0 +1,18 @@
+from analysis import *
+import nltk
+from nltk import word_tokenize
+
+def process(uj):
+    sentence = uj.submissions
+    tokens = word_tokenize(sentence[1].text)
+    print(tokens)
+
+
+uj = UserJournal(phone_number="183402")
+
+s0 = Submission(phone_number="183402", text="I am really loving this")
+s1 = Submission(phone_number="183402", text="I can't stand this right now.")
+
+uj.add_submission(s0)
+uj.add_submission(s1)
+process(uj)
