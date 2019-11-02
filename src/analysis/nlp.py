79c6547e1a4722cabb33164a2f19bdd3a244d54d
tupleu <57295844+tupleu@users.noreply.github.com>
2019-11-02 17:56:46 -0400

    finds nouns

--- src/analysis/nlp.py
+++ src/analysis/nlp.py
@@ -3,20 +3,22 @@ import nltk
 
 def process(uj):
     submissions = uj.submissions
-    tokens = []
-    tags = []
+    alltokens = []
     for i in submissions:
-        tokens += nltk.word_tokenize(i.text)
-        tags += nltk.pos_tag(tokens)
+        tokens = nltk.word_tokenize(i.text)
+        alltokens += tokens
+    tags = nltk.pos_tag(alltokens)
     for i in tags:
-        print(i[1])
+        if i[1] == "NN" or i[1] == "NNS":
+            print(i)
+    
     #for word in tokens:
     #    sum += dic[word]
     #return sum
 uj = UserJournal(phone_number="183402")
 
-s0 = Submission(text="I am really loving this") 
-s1 = Submission(text="I can't stand this right now.")
+s0 = Submission(text="I am very lonely") 
+s1 = Submission(text="My life is not fun")
 
 uj.add_submission(s0)
 uj.add_submission(s1)
