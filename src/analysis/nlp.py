6e8300f9ac00fa62018cce2b8b0124c2bc85fb64
tupleu <57295844+tupleu@users.noreply.github.com>
2019-11-02 16:54:54 -0400

    added tags to tokens

--- src/analysis/nlp.py
+++ src/analysis/nlp.py
@@ -2,20 +2,22 @@ from analysis import *
 import nltk
 
 def process(uj):
-    sentence = uj.submissions
-    for i in sentence:
-        tokens = nltk.word_tokenize(i.text)
-        print(tokens)
-        tagged = nltk.pos_tag(tokens)
-        print(tagged)
-
-
+    submissions = uj.submissions
+    tokens = []
+    tags = []
+    for i in submissions:
+        tokens += nltk.word_tokenize(i.text)
+        tags += nltk.pos_tag(tokens)
+    for i in tags:
+        print(i[1])
+    #for word in tokens:
+    #    sum += dic[word]
+    #return sum
 uj = UserJournal(phone_number="183402")
 
-s0 = Submission(text="I am really loving this")
+s0 = Submission(text="I am really loving this") 
 s1 = Submission(text="I can't stand this right now.")
 
 uj.add_submission(s0)
 uj.add_submission(s1)
-
 process(uj)
