102d6c89e89e54646173254fcd4f377e8b6dae2b
Andres Orbe <aeo34@njit.edu>
2019-11-02 14:37:05 -0400

    dictionary add and submission, not yet test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
+from analysis import *
 
 app = Flask(__name__)
+
+
+UJ_Dict = dict()
+
 
 @app.route('/')
 def home():
@@ -11,6 +16,14 @@ def home():
 def sms():
     number = request.form['From']
     message_body = request.form['Body']
+
+    sub = Submission(phone_number=number, text=message_body)
+    UJ = UserJournal(phone_number=number)
+
+    UJ.add_submission(submission=sub)
+
+    UJ_Dict[number] = UJ
+
 
     resp = MessagingResponse()
     resp.message('Hello {}, you said: {}'.format(number, message_body))
