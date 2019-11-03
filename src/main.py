cab6ec8f1cabf7f493dbe3cebbc08a2b314148b4
pathfold <52586263+pathfold@users.noreply.github.com>
2019-11-02 23:08:15 -0400

    first time msg

--- src/main.py
+++ src/main.py
@@ -15,15 +15,19 @@ def sms():
     number = request.form['From']
     message_body = request.form['Body']
 
-    sub = an.Submission(phone_number=number, text=message_body)
-    UJ = an.UserJournal(phone_number=number)
+    first_time = (number in UJ_Dict.keys)
+    resp = ''
 
-    UJ.add_submission(submission=sub)
+    if first_time:
+        sub = an.Submission(phone_number=number, text=message_body)
+        UJ = an.UserJournal(phone_number=number)
+        UJ.add_submission(submission=sub)
+        UJ_Dict[number] = UJ
+        resp = MessagingResponse()
+        resp.message("Hey, from what I can tell, this is your first time using Mirrur. That's ok. \n Mirrur is a place for you to jot down any of your thoughts. Mirrur is here to listen.")
+    else:
 
-    UJ_Dict[number] = UJ
 
-    resp = MessagingResponse()
-    resp.message('number:{} \n UJ: {}'.format(number, UJ_Dict[number]))
     return str(resp)
 
 if __name__ == '__main__':
