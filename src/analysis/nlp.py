5a8b1127a3bc98ac57cf0d5eaefc19e81204eaad
Andres Orbe <aeo34@njit.edu>
2019-11-02 20:33:50 -0400

    Sentiment Analysis

--- src/analysis/nlp.py
+++ src/analysis/nlp.py
@@ -1,5 +1,26 @@
 from analysis import *
-import nltk
+# import nltk
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+
+
+client = language.LanguageServiceClient.from_service_account_file('/Users/andresorbe/PycharmProjects/Hackathon/linus-68f6aad97157.json')
+
+# The text to analyze
+text = u'WORDS'
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+"""
 
 def process(uj):
     submissions = uj.submissions
@@ -10,9 +31,12 @@ def process(uj):
         tags += nltk.pos_tag(tokens)
     for i in tags:
         print(i[1])
+
     #for word in tokens:
     #    sum += dic[word]
     #return sum
+
+
 
 
 uj = UserJournal(phone_number="183402")
@@ -23,3 +47,4 @@ s1 = Submission(text="I can't stand this right now.")
 uj.add_submission(s0)
 uj.add_submission(s1)
 process(uj)
+"""
